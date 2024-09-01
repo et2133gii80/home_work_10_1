f11f3be08d0da10a8bcc44c6028eea9ec68e3e74
Ilya Gaifullin <proskmax@yandex.ru>
2024-09-01 22:09:39 +0500

    home_work_done

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,11 @@
-def get_mask_card_number(card_number):
+from typing import Union
+
+
+def get_mask_card_number(card_number: Union[str]) -> str:
     """Функция маскировки номера банковской карты"""
-    x = str(card_number)
-    return x[:6] + (len(x) - 10) * "*" + x[-4:]
+    return card_number[:6] + (len(card_number) - 10) * "*" + card_number[-4:]
 
 
-def get_mask_account(account):
+def get_mask_account(account: Union[str]) -> str:
     """Функция маскировки номера банковского счета"""
-    s = str(account)
-    return "**" + s[-4:]
+    return "**" + account[-4:]
--- tests/test_1.py
+++ tests/test_1.py
@@ -2,4 +2,4 @@ from src.masks import get_mask_card_number
 
 """Тест функции маскировки номера банковской карты"""
 
-print(get_mask_card_number(str(7000792289606361)))
+print(get_mask_card_number(str("7000792289606361")))
