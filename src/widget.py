26bc3376767b811c2c61006001da43d103473165
Ilya Gaifullin <proskmax@yandex.ru>
2024-09-07 00:06:55 +0500

    main_commit

--- src/widget.py
+++ src/widget.py
@@ -1,27 +1,23 @@
 from typing import Union
 
+from src.masks import get_mask_account, get_mask_card_number
+
 
 def mask_account_card(account_card: Union[str]) -> str:
-    """Фуникция, которая умеет обрабатывать информацию как о картах, так и о счетах."""
-
-    if account_card[0:4] == "Счет":
-        return "Cчет" + " " + "**" + account_card[21:]
+    """Фукнция маскировки карт и счетов"""
+    if len(account_card.split()[-1]) == 16:
+        new_account_card = get_mask_card_number(account_card.split()[-1])
+        result = f"{account_card[:-16]}{new_account_card}"
+    elif len(account_card.split()[-1]) == 20:
+        new_number = get_mask_account(account_card.split()[-1])
+        result = f"{account_card[:-20]}{new_number}"
     else:
-        return (
-            account_card[:-16]
-            + " "
-            + account_card[-16:-12]
-            + " "
-            + account_card[-12:-10]
-            + "**"
-            + " "
-            + "****"
-            + " "
-            + account_card[-4:]
-        )
+        return "Некорректные данные"
+    return result
 
 
 def get_date(date: Union[str]) -> str:
     """Функция, которая принимает на вход строку с датой в формате "2024-03-11T02:26:18.671407"
     и возвращает строку с датой в формате "ДД.ММ.ГГГГ" """
-    return date[8:10] + "." + date[5:7] + "." + date[0:4]
+    new_date = date[0:10].split("-")
+    return ".".join(new_date[::-1])
--- tests/test_2.py
+++
@@ -1,5 +0,0 @@
-from src.masks import get_mask_account
-
-"""Тест функции маскировки номера банковского счета"""
-
-print(get_mask_account("7000792289606361"))
