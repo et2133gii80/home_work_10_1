23f7e622b3e146f9f459cbdff1eb2bcd66498267
Ilya Gaifullin <proskmax@yandex.ru>
2024-09-06 23:53:49 +0500

    delete_old_files_1_1

---
+++ src/masks.py
@@ -0,0 +1,17 @@
+from typing import Union
+
+
+def get_mask_card_number(card_number: Union[str]) -> str:
+    """Функция маскировки номера банковской карты"""
+    if len(card_number) == 16 and card_number.isdigit():
+        return card_number[:6] + (len(card_number) - 10) * "*" + card_number[-4:]
+    else:
+        return "Некорректные данные"
+
+
+def get_mask_account(account: Union[str]) -> str:
+    """Функция маскировки номера банковского счета"""
+    if len(account) == 20 and account.isdigit():
+        return "**" + account[-4:]
+    else:
+        return "Некорректные данные"
