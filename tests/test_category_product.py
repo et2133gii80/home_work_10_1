86796dde378c1b5dec27c27e6d7ca834db477c6d
Ilya Gaifullin <proskmax@yandex.ru>
2024-10-14 16:22:28 +0500

    первая дз по ооп(исправ.)

--- tests/test_category_product.py
+++ tests/test_category_product.py
@@ -1,6 +1,8 @@
 import pytest
 from src.product import Product
 from src.category import Category
+
+
 @pytest.fixture
 def first_category():
     return Category(
@@ -9,8 +11,8 @@ def first_category():
         list_product=[
             Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5),
             Product("Iphone 15", "512GB, Gray space", 210000.0, 8),
-            Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
-        ]
+            Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14),
+        ],
     )
 
 
@@ -19,26 +21,30 @@ def second_category():
     return Category(
         name="Телевизоры",
         description="Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
-        list_product=[
-            Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
-        ]
+        list_product=[Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)],
     )
 
-def test_category_init(first_category, second_category):
-    assert first_category.name == "Смартфоны"
-    assert first_category.description == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
-    assert len(first_category.list_product) == 3
 
+def test_category_init_category_count_and_product_count(first_category, second_category):
+    assert first_category.name == "Смартфоны"
+    assert (
+        first_category.description
+        == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
+    )
+    assert len(first_category.list_product) == 3
+    # подсчет количества категорий
     assert first_category.category_count == 2
+    # подсчет количества продуктов
     assert second_category.product_count == 4
 
 
 @pytest.fixture
 def product():
-    return Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
+    return Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
+
 
 def test_product(product):
-    assert product.name == "55\" QLED 4K"
+    assert product.name == '55" QLED 4K'
     assert product.description == "Фоновая подсветка"
     assert product.price == 123000.0
     assert product.quantity == 7
