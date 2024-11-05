795d492c808a77d71bb80ef6a73feccbcd3e4a15
Ilya Gaifullin <proskmax@yandex.ru>
2024-11-05 19:50:08 +0500

    6 дз по ооп

--- tests/conftest.py
+++ tests/conftest.py
@@ -256,3 +256,12 @@ def product1():
 @pytest.fixture
 def abc():
     return Test_abc("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+
+
+@pytest.fixture
+def category_zero():
+    return Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        "",
+    )
